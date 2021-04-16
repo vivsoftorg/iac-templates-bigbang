962257338d49842f741fe34f877a4a7779b4457f
Noel Georgi <git@frezbo.dev>
2021-04-16 18:33:10 +0530

    Re-factor

--- deploy.sh
+++ deploy.sh
@@ -4,51 +4,16 @@ set -eu -o pipefail
 
 # Deploy flux and wait for it to be ready
 echo "Installing Flux"
-flux --version
 flux check --pre
 
-# create flux namespace
-kubectl create ns flux-system || true
-
-# delete flux private-registry secret
-kubectl delete secret private-registry -n flux-system || true
-
-# create flux private-registry secret
-kubectl create secret docker-registry private-registry -n flux-system \
-   --docker-server=registry1.dso.mil \
-   --docker-username=${REGISTRY_USERNAME} \
-   --docker-password=${REGISTRY_PASSWORD} || true
-
+# install PSP and StorageClass
+kubectl apply -k cluster-init/
+# create private registry credentials
+sops -d bigbang/envs/dev/secrets/private-registry.enc.yaml | kubectl apply -f -
 # install flux
-kubectl apply -f ./pb-reqs/flux.yaml
-
-# # wait for flux
+kubectl apply -k flux/
 flux check
-
-# Create bigbang namespace
-kubectl apply -f ./pb-reqs/namespace.yaml
-
-# create git repository credentials
-kubectl delete secret repository-credentials -n bigbang || true
-kubectl create secret generic repository-credentials -n bigbang \
-   --from-literal=username=${REPO_USER} \
-   --from-literal=password=${REPO_PASSWORD}
-
-# # deploy bigbang
+# create repository credentials
+sops -d bigbang/envs/dev/secrets/repository-credentials.enc.yaml | kubectl apply -f -
+# install bigbang
 kustomize build bigbang/envs/dev/ | kubectl apply -f -
-
-# # deploy BigBang using dev sized scaling
-# echo "Installing BigBang"
-# helm upgrade -i bigbang chart -n bigbang --create-namespace \
-# --set registryCredentials[0].username='robot$p1-dev' --set registryCredentials[0].password=${REGISTRY1_PASSWORD} \
-# --set registryCredentials[0].registry=registry1.dso.mil \
-# -f values.yaml
-
-# # apply secrets kustomization pointing to current branch
-# echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
-# if [[ -z "${CI_COMMIT_TAG}" ]]; then
-#   cat tests/ci/shared-secrets.yaml | sed 's|master|'$CI_COMMIT_REF_NAME'|g' | kubectl apply -f -
-# else
-#   # NOTE: $CI_COMMIT_REF_NAME = $CI_COMMIT_TAG when running on a tagged build
-#   cat tests/ci/shared-secrets.yaml | sed 's|branch: master|tag: '$CI_COMMIT_REF_NAME'|g' | kubectl apply -f -
-# fi
