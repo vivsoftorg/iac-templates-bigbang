ce3f86ea16a926e34b141c854d3873a37dab1352
Noel Georgi <git@frezbo.dev>
2021-09-16 15:23:10 +0530

    fix: wait for kialis during cleanup

--- destroy.sh
+++ destroy.sh
@@ -9,6 +9,13 @@ until [[ $(kubectl --namespace istio-system get all --no-headers) == "" ]]; do
     sleep 5
 done
 
+kubectl --namespace kiali delete kialis.kiali.io kiali
+
+until [[ $(kubectl --namespace kiali get kialis.kiali.io --no-headers) == "" ]]; do
+    echo "Waiting for cleanup of kiali..."
+    sleep 5
+done
+
 kustomize build bigbang/envs/dev/ | kubectl delete -f -
 
 until [[ $(kubectl --namespace bigbang get helmreleases.helm.toolkit.fluxcd.io --no-headers) == "" ]]; do
